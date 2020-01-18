63426b74acd6418215a27c96d1700265d434e8cb
hainv <hai213k57@gmail.com>
2020-01-18 12:05:09 +0700

    Code function detect error2
    Signed-off-by: hainv <hai213k57@gmail.com>

--- detecterror-example/handler_request.py
+++ detecterror-example/handler_request.py
@@ -108,7 +108,10 @@ def handle_detect(img):
 
     # check loss circle
     if len(possibles) != 8:
-        return "error1"
+        result = {
+            "type": "error1"
+        }
+        return result
 
     # load image examples
     example_possibles = None
@@ -125,18 +128,71 @@ def handle_detect(img):
     # check contours after resize image by image exmaples
     thresh = convert_image_to_thresh(all_possible)
     cv2.imwrite("output/thresh_error2.png", thresh)
-    img_contours, possibles, max_area_possible = find_contours(rotationed)
+    img_contours, detect_possibles, max_area_possible = find_contours(
+        rotationed)
     examples = example_possibles.possibles
     is_error2 = False
-    # for i in range(0, len(examples)):
-    #     examples_area = examples[i].bounding_react_area
-    #     print("Example: ", str(examples[i].bounding_react_area))
-    #     flag
-    #     for j in range(0, len(possibles)):
-    #         possbile_area = possibles[j].bounding_react_area
-    #         sub_area = abs(examples_area-possbile_area)
-    #         if sub_area
-    #         print("Comparse: ", str(possibles[j].bounding_react_area))
 
-    print("Len ==== =====: ", str(len(possibles)))
-    return None
+    sorted_ctrs_examples = sorted(
+        examples, key=lambda ctr: cv2.boundingRect(ctr.contour)[1])
+    sorted_ctrs_detect_possibles = sorted(
+        detect_possibles, key=lambda ctr: cv2.boundingRect(ctr.contour)[1])
+
+    # for i in range(0, len(sorted_ctrs_examples)):
+    #     print("x:", str(sorted_ctrs_examples[i].bounding_react_x))
+    #     print("y:", str(sorted_ctrs_examples[i].bounding_react_y))
+    #     cv2.imwrite("output/sorted_"+str(i)+".png",
+    #                 sorted_ctrs_examples[i].roi)
+    #     print("=================")
+    error_possibles = []
+    print("*** Calculator area errror ***")
+    print("* Total contours: ", str(len(detect_possibles)))
+    print("-----------------------------------")
+    for i in range(0, len(sorted_ctrs_detect_possibles)):
+        # print("x:", str(sorted_ctrs_detect_possibles[i].bounding_react_x))
+        # print("y:", str(sorted_ctrs_detect_possibles[i].bounding_react_y))
+        # cv2.imwrite("output/sorted_detect_"+str(i)+".png",
+        #             sorted_ctrs_detect_possibles[i].roi)
+        area_detect = sorted_ctrs_detect_possibles[i].bounding_react_area
+        area_examples = sorted_ctrs_examples[i].bounding_react_area
+        sub_area = abs(area_detect-area_examples)
+        error = False
+        if(sub_area > 1000):
+            error_possibles.append(sorted_ctrs_detect_possibles[i])
+            error = True
+        print("* index: ", str(i))
+        print("* are example", str(i) + ": "+str(area_examples))
+        print("* are detect", str(i) + ": "+str(area_detect))
+        print("* sub area: ", str(sub_area))
+        print("* error: ", str(error))
+        print("------------------------------------")
+
+    if len(error_possibles) == 0:
+        result = {
+            "type": "good"
+        }
+        return result
+
+    # draw rect error
+    for i in range(0, len(error_possibles)):
+        w = error_possibles[i].bounding_react_with
+        h = error_possibles[i].bounding_react_height
+        x_min = error_possibles[i].bounding_react_x
+        y_min = error_possibles[i].bounding_react_y
+        x_max = x_min + w
+        y_max = y_min + h
+        # print("x: ", str(x_min))
+        # print("x_max", str(x_max))
+        cv2.rectangle(img_contours, (x_min, y_min),
+                      (x_max, y_max), (0, 0, 255), 2)
+    cv2.imwrite("output/result_detect_error.png", img_contours)
+
+    # convert image to base64
+    ret, buffer = cv2.imencode('.jpg', img_contours)
+    jpg_as_text = base64.b64encode(buffer)
+    # print(jpg_as_text)
+    result = {
+        "type": "error2",
+        "image": jpg_as_text
+    }
+    return result
