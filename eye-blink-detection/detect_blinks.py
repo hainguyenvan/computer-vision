dc257e610efd0a60bb091bdda9fabba567ca6b9f
hainv <hai213k57@gmail.com>
2020-02-21 12:05:35 +0700

    Code example eye blink detection
    Signed-off-by: hainv <hai213k57@gmail.com>

--- eye-blink-detection/detect_blinks.py
+++ eye-blink-detection/detect_blinks.py
@@ -30,3 +30,84 @@ def eye_aspect_ratio(eye):
 
     # return the eye aspect ratio
     return ear
+
+
+EYE_AR_THRESH = 0.3
+EYE_AR_CONSEC_FRAMES = 3
+SHAPE_PREDICTOR = "shape_predictor_68_face_landmarks.dat"
+
+
+def main():
+    COUNTER = 0
+    TOTAL = 0
+
+    print("[INFO] loading facial landmark predictor ...")
+    detector = dlib.get_frontal_face_detector()
+    predictor = dlib.shape_predictor(SHAPE_PREDICTOR)
+
+    # grab the indexs of the facial landmarks for the left and
+    # right eye, respectively
+    (l_start, l_end) = face_utils.FACIAL_LANDMARKS_IDXS["left_eye"]
+    (r_start, r_end) = face_utils.FACIAL_LANDMARKS_IDXS["right_eye"]
+
+    # start the video stream thread
+    print("[INFO] starting video stream thread ...")
+    vs = VideoStream(src=0).start()
+    time.sleep(1.0)
+
+    while True:
+        # if not vs.more():
+        #     break
+        frame = vs.read()
+        frame = imutils.resize(frame, width=450)
+        gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # detect faces in the graysacle frame
+        rects = detector(gray, 0)
+
+        # loop over the face detections
+        for rect in rects:
+            shape = predictor(gray, rect)
+            shape = face_utils.shape_to_np(shape)
+
+            left_eye = shape[l_start:l_end]
+            right_eye = shape[r_start:r_end]
+
+            left_ear = eye_aspect_ratio(left_eye)
+            right_ear = eye_aspect_ratio(right_eye)
+
+            # averag the eye aspect ratio together for both eyes
+            ear = (left_ear + right_ear) / 2.0
+
+            left_eye_hull = cv2.convexHull(left_eye)
+            right_eye_hull = cv2.convexHull(right_eye)
+            cv2.drawContours(frame, [left_eye_hull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [right_eye_hull], -1, (0, 255, 0), 1)
+
+            # check to see if eye aspect ratio is below the blink
+            # threshold, and if so, increment the blink frame counter
+            if ear < EYE_AR_THRESH:
+                COUNTER += 1
+            else:
+                if COUNTER >= EYE_AR_CONSEC_FRAMES:
+                    TOTAL += 1
+                COUNTER = 0
+
+            # draw the total number of blinks on the frame along with
+            # the computed eye aspect ratio for the frame
+            cv2.putText(frame, "Blinks: {}".format(TOTAL), (10, 30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+            cv2.putText(frame, "Ear: {}".format(ear), (300,  30),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+
+        # show frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the 'q' key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+
+if __name__ == "__main__":
+    main()
